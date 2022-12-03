ad44721ba5360820b487fe7c57bc2fbe101dd2fe
Feride_queen <faridka0@gmail.com>
2022-12-03 10:35:17 +0300

    3 задание

---
+++ Work.Start/Work2_Start/Work3_Start/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a % 2 == 1)
+{
+    Console.Write("число" + a + " является: нечетным");
+}
+else
+{
+   Console.Write("число" + a + " является: четным");
+
+}
+Console.ReadLine();
